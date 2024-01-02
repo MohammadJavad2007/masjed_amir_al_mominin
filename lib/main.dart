7af7a5c051a423afc301d9b4f50ed1043a0f770d
MohammadJavad2007 <114733800+MohammadJavad2007@users.noreply.github.com>
2024-01-02 19:41:32 +0330

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,322 +1,85 @@
-import 'package:black_hole_flutter/black_hole_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:swipeable_page_route/swipeable_page_route.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: '🔙 swipeable_page_route example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: FirstPage(),
+      debugShowCheckedModeBanner: false,
+      title: 'مسجد امیر المونین',
+      theme: ThemeData(primarySwatch: Colors.green, useMaterial3: false),
+      home: HomeView(),
+      locale: Locale('fa', 'IR'), // استفاده از زبان فارسی
+      fallbackLocale: Locale('fa', 'IR'), // زبان پشتیبانی شده در صورت نیاز
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class HomeController extends GetxController {
+  var counter = 0.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    // انجام تنظیمات اولیه
+    print('کنترلر آماده است.');
+  }
+  
+
+  void increment() {
+    counter.value++;
+    print(counter);
+  }
+}
+
+
+
+class HomeView extends GetView<HomeController> {
+
+  final HomeController controller = Get.put(HomeController());
+  final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Drawer(),
+      key: _key, // Assign the key to Scaffold.
       appBar: AppBar(
-        title: Text('hello'),
+        title: Align(alignment: Alignment.centerLeft, child: Text('ایتا')),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              onPressed: () => _key.currentState!.openEndDrawer(),
+              icon: Icon(Icons.menu),
+            ),
+          ),
+        ],
+        leading: Icon(Icons.search),
       ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              showGeneralDialog(
-                barrierLabel: "Label",
-                barrierDismissible: false,
-                barrierColor: const Color.fromARGB(0, 255, 255, 255),
-                transitionDuration: Duration(milliseconds: 400),
-                context: context,
-                pageBuilder: (context, anim1, anim2) {
-                  return Screen();
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('counter'),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 300,
+              height: 130,
+              child: ElevatedButton(
+                onPressed: () {
+                  controller.increment();
                 },
-                transitionBuilder: (context, anim1, anim2, child) {
-                  return SlideTransition(
-                    position: Tween(begin: Offset(1, 0), end: Offset(0, 0))
-                        .animate(anim1),
-                    child: child,
-                  );
-                },
-              );
-            },
-            child: Text('GO')),
-      ),
-    );
-  }
-}
-
-class Screen extends StatelessWidget {
-  const Screen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      onDismissed: (context) {
-        Get.back();
-      },
-      direction: DismissDirection.startToEnd,
-      key: Key('hello5'),
-      child: Material(
-        elevation: 20 ,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Dismissible'),
-          ),
-          body: Center(child: Text('Hi')),
+                child: Text('Increment'),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// class FirstPage extends StatefulWidget {
-//   @override
-//   State<FirstPage> createState() => _FirstPageState();
-// }
-
-// class _FirstPageState extends State<FirstPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     // final pageRoute = context.getSwipeablePageRoute<void>()!;
-//      return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Page 2'),
-//         actions: [
-//           IconButton(
-//             key: const ValueKey('check'),
-//             icon: const Icon(Icons.check),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('star'),
-//             icon: const Icon(Icons.star),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('play_arrow'),
-//             icon: const Icon(Icons.play_arrow),
-//             onPressed: () {},
-//           ),
-//           PopupMenuButton<void>(
-//             itemBuilder: (context) => [
-//               const PopupMenuItem(child: Text('One')),
-//               const PopupMenuItem(child: Text('Two')),
-//             ],
-//           ),
-//         ],
-//       ),
-//       body: SizedBox.expand(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             // Text('Can swipe: ${pageRoute.canSwipe}'),
-//             TextButton(
-//               onPressed: () {
-//                 // You can disable swiping completely using `canSwipe`:
-//                 setState(() {});
-//               },
-//               child: const Text('Toggle'),
-//             ),
-//             // Text('Can only swipe from edge: ${pageRoute.canOnlySwipeFromEdge}'),
-//             TextButton(
-//               onPressed: () => setState(
-//                 () => {}
-//               ),
-//               child: const Text('Toggle'),
-//             ),
-//             const SizedBox(height: 32),
-//             ElevatedButton(
-//               onPressed: () {
-//                 context.navigator.push<void>(SwipeablePageRoute(
-//                   // You can customize the width of the detection area with
-//                   // `backGestureDetectionWidth`.
-//                   // transitionBuilder: (context, animation, secondaryAnimation, isSwipeGesture, child) {
-//                   //   return ScaleTransition(scale: animation,);
-//                   // },
-//                   builder: (_) => SecondPage(),
-//                 ));
-//               },
-//               child: const Text('Open page 3'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class SecondPage extends StatefulWidget {
-//   @override
-//   State<SecondPage> createState() => _SecondPageState();
-// }
-
-// class _SecondPageState extends State<SecondPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     // Gets the `SwipeablePageRoute` wrapping the current page.
-//     final pageRoute = context.getSwipeablePageRoute<void>()!;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Page 2'),
-//         actions: [
-//           IconButton(
-//             key: const ValueKey('check'),
-//             icon: const Icon(Icons.check),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('star'),
-//             icon: const Icon(Icons.star),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('play_arrow'),
-//             icon: const Icon(Icons.play_arrow),
-//             onPressed: () {},
-//           ),
-//           PopupMenuButton<void>(
-//             itemBuilder: (context) => [
-//               const PopupMenuItem(child: Text('One')),
-//               const PopupMenuItem(child: Text('Two')),
-//             ],
-//           ),
-//         ],
-//       ),
-//       body: SizedBox.expand(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text('Can swipe: ${pageRoute.canSwipe}'),
-//             TextButton(
-//               onPressed: () {
-//                 // You can disable swiping completely using `canSwipe`:
-//                 setState(() => pageRoute.canSwipe = !pageRoute.canSwipe);
-//               },
-//               child: const Text('Toggle'),
-//             ),
-//             Text('Can only swipe from edge: ${pageRoute.canOnlySwipeFromEdge}'),
-//             TextButton(
-//               onPressed: () => setState(
-//                 () => pageRoute.canOnlySwipeFromEdge =
-//                     !pageRoute.canOnlySwipeFromEdge,
-//               ),
-//               child: const Text('Toggle'),
-//             ),
-//             const SizedBox(height: 32),
-//             ElevatedButton(
-//               onPressed: () {
-//                 context.navigator.push<void>(SwipeablePageRoute(
-//                   // You can customize the width of the detection area with
-//                   // `backGestureDetectionWidth`.
-//                   builder: (_) => ThirdPage(),
-//                 ));
-//               },
-//               child: const Text('Open page 3'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class ThirdPage extends StatefulWidget {
-//   @override
-//   State<ThirdPage> createState() => _ThirdPageState();
-// }
-
-// class _ThirdPageState extends State<ThirdPage>
-//     with SingleTickerProviderStateMixin {
-//   static const _tabCount = 3;
-//   late final TabController _tabController;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _tabController = TabController(length: _tabCount, vsync: this);
-//     _tabController.addListener(() {
-//       if (!mounted) return;
-
-//       final canSwipe = _tabController.index == 0;
-//       context.getSwipeablePageRoute<void>()!.canSwipe = canSwipe;
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     _tabController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: MorphingAppBar(
-//         backgroundColor: Colors.green,
-//         title: const Text('Page 3'),
-//         actions: [
-//           IconButton(
-//             key: const ValueKey('star'),
-//             icon: const Icon(Icons.star),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('play_arrow'),
-//             icon: const Icon(Icons.play_arrow),
-//             onPressed: () {},
-//           ),
-//           IconButton(
-//             key: const ValueKey('favorite'),
-//             icon: const Icon(Icons.favorite),
-//             onPressed: () {},
-//           ),
-//           PopupMenuButton<void>(
-//             itemBuilder: (context) => [
-//               const PopupMenuItem(child: Text('One')),
-//               const PopupMenuItem(child: Text('Two')),
-//             ],
-//           ),
-//         ],
-//         bottom: TabBar(
-//           controller: _tabController,
-//           indicatorColor: Colors.white,
-//           isScrollable: true,
-//           tabs: [
-//             for (var i = 0; i < _tabCount; i++) Tab(text: 'Tab ${i + 1}'),
-//           ],
-//         ),
-//       ),
-//       body: TabBarView(
-//         controller: _tabController,
-//         children: [
-//           for (var i = 0; i < _tabCount; i++)
-//             Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text('This is tab ${i + 1}'),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     context.navigator.push<void>(
-//                       SwipeablePageRoute(builder: (_) => SecondPage()),
-//                     );
-//                   },
-//                   child: const Text('Open page 2'),
-//                 ),
-//               ],
-//             ),
-//         ],
-//       ),
-//     );
-//   }
-// }
